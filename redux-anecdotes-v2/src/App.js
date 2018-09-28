fc5e757da14dad84dc09df0d40456d2766a3ee06
Pasi Junttila <pasi.junttila@live.com>
2018-09-28 17:34:03 +0300

    Tehtava 6.10 anekdootit ja backend, osa 1: getAll.

--- redux-anecdotes-v2/src/App.js
+++ redux-anecdotes-v2/src/App.js
@@ -2,9 +2,15 @@ import React from 'react';
 import Notification from './components/Notification';
 import AnecdoteForm from './components/AnecdoteForm';
 import AnecdoteList from './components/AnecdoteList';
+import { connect } from 'react-redux';
+import { initAnecdotes } from './reducers/anecdoteReducer';
+import anecdoteService from './services/anecdotes';
 
 class App extends React.Component {
-
+  componentDidMount = async () => {
+    const response = await anecdoteService.getAll();
+    this.props.initAnecdotes(response);
+  }
   render() {
     return (
       <div>
@@ -17,4 +23,8 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = {
+  initAnecdotes: initAnecdotes
+};
+const ConnectedApp = connect(null, mapDispatchToProps)(App);
+export default ConnectedApp;
--- redux-anecdotes-v2/src/reducers/anecdoteReducer.js
+++ redux-anecdotes-v2/src/reducers/anecdoteReducer.js
@@ -1,34 +1,17 @@
-const anecdotesAtStart = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-];
 
 const getId = () => (100000*Math.random()).toFixed(0);
 
-const asObject = (anecdote) => {
-  return {
-    content: anecdote,
-    id: getId(),
-    votes: 0
-  };
-};
-
-const initialState = anecdotesAtStart.map(asObject);
-
-const reducer = (store = initialState, action) => {
+const reducer = (store = [], action) => {
   if (action.type==='VOTE') {
     const old = store.filter(a => a.id !==action.id);
     const voted = store.find(a => a.id === action.id);
-
     return [...old, { ...voted, votes: voted.votes+1 } ];
   }
-  if (action.type === 'CREATE') {
-
+  else if (action.type === 'CREATE') {
     return [...store, { content: action.content, id: getId(), votes:0 }];
+  }
+  else if (action.type === 'INIT_STORE') {
+    return action.data;
   }
 
   return store;
@@ -45,6 +28,13 @@ export const voteAnecdote = (id) => {
   return {
     type: 'VOTE',
     id: id
+  };
+};
+
+export const initAnecdotes = (data) => {
+  return {
+    type: 'INIT_STORE',
+    data: data
   };
 };
 
